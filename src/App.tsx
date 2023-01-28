26b00af24e372efba625ec87ce375c5e4ad37f28
Rasel Karim <raselkarim7@gmail.com>
2023-01-28 23:18:44 +0600

    fancy sidebar menu item added

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import "./App.css";
-
+import MenuOpenIcon from "@mui/icons-material/MenuOpen";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [sidebar, setSidebar] = useState(true);
 
   useEffect(() => {}, []);
+  const menuArray = Array(5).fill(0);
 
   return (
     <div className="App">
       <div className="header">
         <div className="part1">
-          Part 1
-          
+          <div className="appTitle">NAGANO NABCO</div>
+          <div className="menuOpenIconContainer">
+            <MenuOpenIcon />
+          </div>
         </div>
-        <div>
-          Part 2
-        </div>
+        <div></div>
       </div>
-      <div className="sidebar">Sidebar</div>
+      <div className="sidebar">
+        {menuArray.map((item, index) => (
+          <div className="menuItem" key={index}>Menu {index + 1} </div>
+        ))}
+      </div>
       <div className="main">Main</div>
     </div>
   );
