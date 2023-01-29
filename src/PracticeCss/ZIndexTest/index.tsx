ede79e72912b0f9a091f8459d90ae4784aee3202
Rasel Karim <raselkarim7@gmail.com>
2023-01-29 14:05:45 +0600

    by default modal false set

--- src/PracticeCss/ZIndexTest/index.tsx
+++ src/PracticeCss/ZIndexTest/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import './ZIndexTest.css'
 
 const ZIndexTest = () => {
-  const [open, setOpen] = React.useState(true);
+  const [open, setOpen] = React.useState(false);
   return (
     <>
       <h2>Modal Example</h2>
