2ba276b46b9401c3c6fc65b62293dbf77c5d1410
Rasel Karim <raselkarim7@gmail.com>
2023-01-29 13:42:59 +0600

    zindex not understood

---
+++ src/PracticeCSS.tsx
@@ -0,0 +1,35 @@
+import React from "react";
+
+const PracticeCSS = () => {
+  const [open, setOpen] = React.useState(true);
+  return (
+    <>
+      <h2>Modal Example</h2>
+
+      <button id="myBtn" onClick={() => setOpen((prev) => !prev)}>
+        Open Modal
+      </button>
+
+      <div className="square beneath1 "></div>
+
+      <div className=" square beneath2 "></div>
+
+      <div
+        id="myModal"
+        className="modal"
+        style={{ display: open ? "block" : "none" }}
+      >
+        <div className="modal-content">
+          <span className="close" onClick={() => setOpen((prev) => !prev)}>
+            &times;
+          </span>
+          <p>Some text in the Modal..</p>
+          <div className="square inside1 "></div>
+          <div className="square inside2 "></div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PracticeCSS;
--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import PracticeCSS from './PracticeCSS'
 
 ReactDOM.createRoot(document.getElementById('app') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <PracticeCSS />
   </React.StrictMode>,
 )
